58501f6628b1998433fafe4c744546559f0d964d
root <armanice09@gmail.com>
2021-02-02 12:03:34 +0600

    3. AJAX GET DATA onload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-let a;
-let b=a;
-a =90;
-document.write(b);
- 
+
+document.getElementById("get_data").addEventListener('click', loadData);
+
+function loadData(){
+   // console.log("Button clicked!");
+
+   //create XHR Object(Xml Http Request)
+   let xhr = new XMLHttpRequest();
+
+
+   // xhr open
+   xhr.open('GET', 'data.txt', true);
+
+   xhr.onload = function(){
+       if (this.status === 200){
+
+        document.getElementById('output').innerHTML=`<h4>${this.responseText}</h4>`
+       }
+   }
+
+   xhr.send();
+
+  // console.log(xhr);
+   
+}
